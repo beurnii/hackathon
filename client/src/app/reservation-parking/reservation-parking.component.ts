0e3fbee79ee81afcbe55e2b25ccbae8e543e6ade
Sebastien Cadorette <cado.94@me.com>
2019-01-19 14:55:54 -0500

    Ajout du template pour le formulaire de reservation.

--- client/src/app/reservation-parking/reservation-parking.component.ts
+++ client/src/app/reservation-parking/reservation-parking.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./reservation-parking.component.scss']
 })
 export class ReservationParkingComponent implements OnInit {
+    // tslint:disable-next-line:no-any
+    public model: any = {};
 
     public constructor() { }
 
     public ngOnInit(): void { }
 
+    public onSubmit(): void {
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+    }
 }
