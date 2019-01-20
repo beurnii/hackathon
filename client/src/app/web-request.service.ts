712bfb35feca93c7a1106a03b170f9cb9b9b31ab
Sebastien Cadorette <cado.94@me.com>
2019-01-19 19:39:30 -0500

    Ajout d'une requete pour la reservation.

--- client/src/app/web-request.service.ts
+++ client/src/app/web-request.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Reservation } from './reservation-parking/reservation-parking.component';
+
+const BASE_URL: string = 'http://localhost:3000/';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class WebRequestService {
 
-  constructor() { }
+    public constructor(private _http: HttpClient) { }
+
+    public async makeReservation(reservation: Reservation): Promise<void> {
+        await this._http.put(BASE_URL + 'reservation/' + reservation.parkingID, reservation);
+    }
 }
