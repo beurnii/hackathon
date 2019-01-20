e5c861b2590315b00fd00ece887836bdcee4800d
Bernard <bern97@hotmail.ca>
2019-01-19 19:37:45 -0500

    place db

--- server/src/BD/MongoDB.ts
+++ server/src/BD/MongoDB.ts
@@ -1,4 +1,4 @@
-import { Mongoose } from 'mongoose';
+import { Mongoose, Schema, Model, Document } from 'mongoose';
 import { injectable } from 'inversify';
 
 const DB_URL: string = 'mongodb://camarois:admin1@ds161794.mlab.com:61794/hackathon';
@@ -6,18 +6,40 @@ const DB_URL: string = 'mongodb://camarois:admin1@ds161794.mlab.com:61794/hackat
 @injectable()
 export class MongoDB {
     private _mongoose: Mongoose;
-    // private _schema: Schema;
-    // private _model: Model<Document>;
+    private _schema: Schema;
+    private _model: Model<Document>;
+
+    get model(): Model<Document> {
+        return this._model;
+    }
 
     constructor() {
         this._mongoose = new Mongoose();
-        // this._schema = new Schema(sch);
-        // this._model = this._mongoose.model(modelName, this._schema);
-        this.connectToBD();
+        this._schema = new Schema({
+          sNoPlace: String,
+          nLongitude: Number,
+          nLatitude: Number,
+          nPositionCentreLongitude: Number,
+          nPositionCentreLatitude: Number,
+          Occupation: Number,
+          sStatut: Number,
+          sGenre: String,
+          sType: String,
+          sAutreTete: String,
+          sNomRue: String,
+          nSupVelo: Number,
+          sTypeExploitation: String,
+          nTarifHoraire: Number,
+          sLocalisation: String,
+          nTarifMax: String
+        });
+
+        this._model = this._mongoose.model('parkingPlaces', this._schema);
+        this.connectToBD().catch();
     }
 
     private async connectToBD(): Promise<void> {
-        await this._mongoose.connect(DB_URL).catch(() => {
+        await this._mongoose.connect(DB_URL, { useNewUrlParser: true }).catch(() => {
             console.log("Error while connecting to the BD.");
         });
     }
--- server/src/routes-parking-data/routes-parking-data.ts
+++ server/src/routes-parking-data/routes-parking-data.ts
@@ -1,30 +1,50 @@
-import { injectable } from 'inversify';
-import { WebService } from '../WebService';
-import { Router, Request, Response } from 'express';
-const csv = require('csvtojson');
+import {injectable} from 'inversify';
+import {WebService} from '../WebService';
+import {Router, Request, Response} from 'express';
+import {MongoDB} from "../BD/MongoDB";
+
 
 @injectable()
 export class RoutesParkingData extends WebService {
 
-  public readonly mainRoute: string;
+    public readonly mainRoute: string;
 
-  public constructor() {
-    super();
-    this.mainRoute = '';
-  }
+    public constructor(private mongoDB: MongoDB = new MongoDB()) {
+        super();
+        this.mainRoute = '';
+    }
 
-  public get routes(): Router {
-    const router: Router = Router();
+    public get routes(): Router {
+        const router: Router = Router();
 
-    router.get('/getParkingData', (req: Request, res: Response) => {
-      csv()
-      .fromFile('./Places.csv')
-      .then((jsonObj: any) => {
-        res.status(200).json(jsonObj);
-      });
-    });
+        router.get('/getParkingData', (req: Request, res: Response) => {
 
-    return router;
-  }
+            // const a = new this.mongoDB.model({
+            //     sNoPlace: 'ert',
+            //     nLongitude: 34,
+            //     nLatitude: 234,
+            //     nPositionCentreLongitude: 234,
+            //     nPositionCentreLatitude: 23,
+            //     Occupation: 234,
+            //     sStatut: 234,
+            //     sGenre: 'efs',
+            //     sType: 'wer',
+            //     sAutreTete: 'wer',
+            //     sNomRue: 'ert',
+            //     nSupVelo: 0,
+            //     sTypeExploitation: 'wwet',
+            //     nTarifHoraire: 234,
+            //     sLocalisation: 'wer',
+            //     nTarifMax: 'wet'
+            // });
+            // a.save().then((err)=>{console.log(err)});
+
+            this.mongoDB.model.find((err, data) =>{
+                res.status(200).json(data);
+            });
+        });
+
+        return router;
+    }
 
 }
