c4fd17cfb85a1e7c685d4d59e7169778f5c855dd
Sebastien Cadorette <cado.94@me.com>
2019-01-20 07:50:32 -0500

    Alignement

--- server/src/routes-parking-data/routes-parking-data.ts
+++ server/src/routes-parking-data/routes-parking-data.ts
@@ -1,10 +1,10 @@
-import {injectable, inject} from 'inversify';
-import {WebService} from '../WebService';
-import {Router, Request, Response} from 'express';
-import {MongoDB} from '../BD/MongoDB';
+import { injectable, inject } from 'inversify';
+import { WebService } from '../WebService';
+import { Router, Request, Response } from 'express';
+import { MongoDB } from '../BD/MongoDB';
 import Types from '../Types';
-import {SocketServerService} from '../socket-io.service';
-import {Document} from 'mongoose';
+import { SocketServerService } from '../socket-io.service';
+import { Document } from 'mongoose';
 
 const OK_STATUS: number = 200;
 
@@ -13,8 +13,8 @@ export class RoutesParkingData extends WebService {
 
     public readonly mainRoute: string;
 
-    public constructor(@inject(Types.SocketServerService) private socket: SocketServerService,
-                       private mongoDB: MongoDB = new MongoDB()) {
+    public constructor( @inject(Types.SocketServerService) private socket: SocketServerService,
+                        private mongoDB: MongoDB = new MongoDB()) {
         super();
         this.mainRoute = '';
     }
@@ -23,25 +23,26 @@ export class RoutesParkingData extends WebService {
         const router: Router = Router();
 
         router.get('/getParkingData', (req: Request, res: Response) => {
-            this.mongoDB.model.find({Occupation: 0}, (err: Error, data: Document) => {
+            this.mongoDB.model.find({ Occupation: 0 }, (err: Error, data: Document) => {
                 res.status(OK_STATUS).json(data);
             });
         });
 
         router.post('/reservation/:id/:time', (req: Request, res: Response) => {
-            this.mongoDB.model.findOneAndUpdate({sNoPlace: req.params.id}, {$set: {Occupation: 1}}).then((parkingSpot: Document) => {
+            this.mongoDB.model.findOneAndUpdate({ sNoPlace: req.params.id }, { $set: { Occupation: 1 } }).then((parkingSpot: Document) => {
                 res.status(OK_STATUS).json(parkingSpot);
                 setTimeout(() => {
-                    this.mongoDB.model.findOneAndUpdate({sNoPlace: req.params.id}, {$set: {Occupation: 0}})
+                    this.mongoDB.model.findOneAndUpdate({ sNoPlace: req.params.id }, { $set: { Occupation: 0 } })
                         .then((parking: Document) => {
                             this.socket.reservationOver(parking);
                         });
+                // tslint:disable-next-line:align
                 }, req.params.time * 6000);
             });
         });
 
         router.post('/reservationAuto/:id/', (req, res) => {
-            this.mongoDB.model.findOneAndUpdate({sNoPlace: req.params.id}, {$set: {Occupation: 1}})
+            this.mongoDB.model.findOneAndUpdate({ sNoPlace: req.params.id }, { $set: { Occupation: 1 } })
                 .then((parkingSpot: Document) => {
                     this.socket.reservation(req.params.id);
                     res.status(OK_STATUS).json(parkingSpot);
@@ -49,7 +50,7 @@ export class RoutesParkingData extends WebService {
         });
 
         router.post('/liberationAuto/:id/', (req, res) => {
-            this.mongoDB.model.findOneAndUpdate({sNoPlace: req.params.id}, {$set: {Occupation: 0}})
+            this.mongoDB.model.findOneAndUpdate({ sNoPlace: req.params.id }, { $set: { Occupation: 0 } })
                 .then((parkingSpot: Document) => {
                     this.socket.reservationOver(parkingSpot);
                     res.status(OK_STATUS).json(parkingSpot);
